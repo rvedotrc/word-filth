e6e0e854e89be73887deda768884857524efad00
Rachel Evans <revans@zendesk.com>
2020-09-25 15:45:20 +0200

    More class->function

--- src/js/components/MyVocab/show_list.tsx
+++ src/js/components/MyVocab/show_list.tsx
@@ -20,55 +20,52 @@ type AggregateItem = {
     flexMatches: boolean;
 }
 
-class ShowList extends React.Component<Props, never> {
+const ShowList = (props: Props) => {
 
-    render() {
-        const { t, vocabList, isDeleting, selectedKeys, onToggleSelected, flexMatchedKeys } = this.props;
+    const { t, vocabList, isDeleting, selectedKeys, onToggleSelected, flexMatchedKeys } = props;
 
-        const cmp = (a: AggregateItem, b: AggregateItem) => {
-            let r = a.vocabRow.sortKey.localeCompare(b.vocabRow.sortKey);
-            if (r === 0) r = (a.vocabEntry.vocabKey).localeCompare(b.vocabEntry.vocabKey);
-            return r;
-        };
+    const cmp = (a: AggregateItem, b: AggregateItem) => {
+        let r = a.vocabRow.sortKey.localeCompare(b.vocabRow.sortKey);
+        if (r === 0) r = (a.vocabEntry.vocabKey).localeCompare(b.vocabEntry.vocabKey);
+        return r;
+    };
 
-        const sortedList: AggregateItem[] = vocabList
-            .map(v => ({
-                vocabEntry: v.vocabEntry,
-                vocabRow: v.vocabRow,
-                isSelected: selectedKeys.has(v.vocabEntry.vocabKey),
-                flexMatches: !flexMatchedKeys || flexMatchedKeys.has(v.vocabEntry.vocabKey || ""),
-            }))
-            .sort(cmp);
+    const sortedList: AggregateItem[] = vocabList
+        .map(v => ({
+            vocabEntry: v.vocabEntry,
+            vocabRow: v.vocabRow,
+            isSelected: selectedKeys.has(v.vocabEntry.vocabKey),
+            flexMatches: !flexMatchedKeys || flexMatchedKeys.has(v.vocabEntry.vocabKey || ""),
+        }))
+        .sort(cmp);
 
-        return (
-            <table id={"MyVocab"}>
-                <thead>
-                <tr>
-                    <th>{t('my_vocab.table.heading.type')}</th>
-                    {isDeleting && <th/>}
-                    <th>{t('my_vocab.table.heading.danish')}</th>
-                    <th>{t('my_vocab.table.heading.english')}</th>
-                    <th>{t('my_vocab.table.heading.details')}</th>
-                    <th>{t('my_vocab.table.heading.tags')}</th>
-                </tr>
-                </thead>
-                <tbody>
-                    {sortedList.map(row => (
-                        <Row
-                            key={row.vocabEntry.vocabKey}
-                            vocabEntry={row.vocabEntry}
-                            vocabRow={row.vocabRow}
-                            isDeleting={isDeleting}
-                            isSelected={row.isSelected}
-                            onToggleSelected={onToggleSelected}
-                            isVisible={row.flexMatches}
-                        />
-                    ))}
-                </tbody>
-            </table>
-        )
-    }
-
-}
+    return (
+        <table id={"MyVocab"}>
+            <thead>
+            <tr>
+                <th>{t('my_vocab.table.heading.type')}</th>
+                {isDeleting && <th/>}
+                <th>{t('my_vocab.table.heading.danish')}</th>
+                <th>{t('my_vocab.table.heading.english')}</th>
+                <th>{t('my_vocab.table.heading.details')}</th>
+                <th>{t('my_vocab.table.heading.tags')}</th>
+            </tr>
+            </thead>
+            <tbody>
+                {sortedList.map(row => (
+                    <Row
+                        key={row.vocabEntry.vocabKey}
+                        vocabEntry={row.vocabEntry}
+                        vocabRow={row.vocabRow}
+                        isDeleting={isDeleting}
+                        isSelected={row.isSelected}
+                        onToggleSelected={onToggleSelected}
+                        isVisible={row.flexMatches}
+                    />
+                ))}
+            </tbody>
+        </table>
+    );
+};
 
 export default withTranslation()(ShowList);
