8c3c53a1ded371aa63e86897355ac581d6ef0513
Rachel Evans <rachel@rachelevans.org>
2020-10-28 23:18:21 +0100

    Babbel prompt

--- src/js/words/Babbel/given_english_question/header.tsx
+++ src/js/words/Babbel/given_english_question/header.tsx
@@ -2,12 +2,23 @@ import * as React from 'react';
 import GivenEnglishQuestion, {T, C} from ".";
 import {QuestionHeaderProps} from "../../CustomVocab/types";
 
+const makePrompt = (q: GivenEnglishQuestion): string => {
+    let prompt = q.englishQuestion;
+
+    if (!q.englishQuestion.match(/^(a|an) /) && q.danishAnswers.some(t => t.match(/^(en|et) /))) {
+        prompt = prompt + " [en/et ...]";
+    }
+
+    return prompt;
+}
+
 const Header = (props: QuestionHeaderProps<T, C, GivenEnglishQuestion>) =>
     <p>
+        [Babbel]{' '}
         {props.t('question.shared.how_do_you_say_in_danish', {
             skipInterpolation: true,
             postProcess: 'pp',
-            english: <b>{props.question.englishQuestion}</b>
+            english: <b>{makePrompt(props.question)}</b>
         })}
     </p>;
 
