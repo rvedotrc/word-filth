1579b2d1d3645a67d96b9a23992ca7896de7590d
Rachel Evans <git@rve.org.uk>
2020-11-28 11:04:45 +0100

    Use both whole-english and also parts for verb VerbumGivenEnglish

--- src/js/words/CustomVocab/verbum/verbum_question_generator.ts
+++ src/js/words/CustomVocab/verbum/verbum_question_generator.ts
@@ -13,19 +13,28 @@ export default class VerbumQuestionGenerator {
 
         if (verb.engelsk && verb.engelsk.startsWith('to ')) {
             q.push(new VerbumGivenEnglish({
-                lang: verb.lang || 'da',
+                lang: verb.lang,
                 english: verb.engelsk,
                 danishAnswers: [verb.infinitiv],
                 vocabSources: [verb],
             }));
         }
 
-        const parts = (verb.engelsk || '').split(/\s*[,;]\s*/).filter(Boolean);
-        parts.filter(part => part.match(/^to (\w+)( \w+)*$/)).map(part => {
+        const engelskParts = (verb.engelsk || '')
+            .split(/\s*[,;]\s*/)
+            .filter(part => part.match(/^to (\w+)( \w+)*$/));
+
+        engelskParts.map(part => {
             q.push(new VerbumGivenDanish({
-                lang: verb.lang || 'da',
+                lang: verb.lang,
                 infinitiv: verb.infinitiv,
                 englishAnswers: [part],
+                vocabSources: [verb],
+            }));
+            q.push(new VerbumGivenEnglish({
+                lang: verb.lang,
+                english: part,
+                danishAnswers: [verb.infinitiv],
                 vocabSources: [verb],
             }));
         });
