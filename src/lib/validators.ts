656df13b5211566843d2b178db2987d6c698d227
Rachel Evans <rachel@rachelevans.org>
2021-01-23 22:44:27 +0100

    Allow 'é'. Consistently disallow '_' in tags.

--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -2,15 +2,16 @@ import {removeParticle} from "lib/particle";
 import * as VocabLanguage from "lib/vocab_language";
 
 export const isSingleWord = (s: string): boolean => {
-    return s.match(/^[abcdefghijklmnopqrstuvwxyzæøå]+$/) !== null;
+    return s.match(/^[abcdefghijklmnopqrstuvwxyzéæøå]+$/) !== null;
 };
 
 export const isSingleWordOrNull = (s: string | null): boolean => {
     return s === null || isSingleWord(s);
 };
 
+// See also parseTags
 export const isTag = (s: string): boolean => {
-    return s.match(/^[abcdefghijklmnopqrstuvwxyzæøå0123456789]+$/) !== null;
+    return s.match(/^[abcdefghijklmnopqrstuvwxyzéæøå0123456789]+$/) !== null;
 };
 
 export const isNonEmptyListOf = <T>(list: T[], predicate: (t: T) => boolean): boolean => {
