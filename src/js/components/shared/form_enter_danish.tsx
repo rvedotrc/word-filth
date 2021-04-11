5c8aae7a813b130abb33b745ac8338eb6211fdba
Rachel Evans <git@rve.org.uk>
2021-04-11 10:47:30 +0200

    Use grid layout for remaining questions

--- src/js/components/shared/form_enter_danish.tsx
+++ src/js/components/shared/form_enter_danish.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
-import {useState} from 'react';
+import {useRef, useState} from 'react';
 import {QuestionFormProps} from "lib/types/question";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const styles = require("./form_enter_x.css");
 
 export type T = {
     dansk: string;
@@ -9,31 +12,32 @@ export type T = {
 const FormEnterDanish = (props: QuestionFormProps<T>) => {
     const [value, setValue] = useState<string>("");
 
-    return (
-        <div>
-            <label>
-                <span>{props.t(`question.shared.label.${props.lang}`)}</span>
-                <input
-                    value={value}
-                    autoFocus={true}
-                    onChange={e => {
-                        let newValue = e.target.value;
-                        setValue(newValue);
-                        newValue = newValue.trim();
+    const idPrefix = useRef(`id-${new Date().getTime()}`);
 
-                        if (newValue !== '') {
-                            props.onAttempt({ dansk: newValue });
-                        } else {
-                            props.onAttempt(undefined);
-                        }
-                    }}
-                    spellCheck={"false"}
-                    autoCapitalize={'none'}
-                    autoComplete={'off'}
-                    autoCorrect={'off'}
-                    lang={props.lang}
-                />
-            </label>
+    return (
+        <div className={styles.grid}>
+            <label htmlFor={idPrefix.current}>{props.t(`question.shared.label.${props.lang}`)}</label>
+            <input
+                id={idPrefix.current}
+                value={value}
+                autoFocus={true}
+                onChange={e => {
+                    let newValue = e.target.value;
+                    setValue(newValue);
+                    newValue = newValue.trim();
+
+                    if (newValue !== '') {
+                        props.onAttempt({ dansk: newValue });
+                    } else {
+                        props.onAttempt(undefined);
+                    }
+                }}
+                spellCheck={"false"}
+                autoCapitalize={'none'}
+                autoComplete={'off'}
+                autoCorrect={'off'}
+                lang={props.lang}
+            />
         </div>
     );
 }
